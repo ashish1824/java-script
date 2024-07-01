f651358c10a834b0bdc908a6a5841959ce89fe28
Ashish <ashishyadav1824@gmail.com>
2024-07-02 00:02:12 +0530

    javascript

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log( 3 
+    + 
+    3) // code readeability should be high
+
+    console.log("Ashish")
+
+
+  let name = "ashish"
+  let age = 18
+  let isLoggedIn = false  
+  let state;
+
+// <<---these are primitive datatype---->>
+
+  // number => 2 to power 51
+  // bigint
+  // string => ""
+  // boolean => true/false
+  // null => standalone value
+  // undefined =>
+
+  // symbol => unique
+  // object
+
+  console.log(typeof "ashish");  // string datatype
+
+  console.log(typeof null);  // ** null is object
+
+  console.log(typeof undefined);  // undefined datatype
