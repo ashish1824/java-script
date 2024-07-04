cb8958b2ddde4fb202d3c2dd127f0bf984e90cba
Ashish <ashishyadav1824@gmail.com>
2024-07-04 23:46:01 +0530

    comp

---
+++ comparison.js
@@ -0,0 +1,12 @@
+// comparison of datatypes
+// console.log("2" < 1); // automatic string convert to number
+
+console.log(null > 0); // false
+console.log(null >= 0); // true
+console.log(null == 0); // false // comparison converted null into number 
+
+
+// === check datatypes and value both
+// == check value
+console.log("2" === 2);
+console.log("2" == 2);
