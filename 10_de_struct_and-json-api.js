b4126668b3ff710804fb3029cc7b55db737e91ff
Ashish <ashishyadav1824@gmail.com>
2024-07-24 00:35:17 +0530

    json

---
+++ 10_de_struct_and-json-api.js
@@ -0,0 +1,32 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "ashish",
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+
+console.log(instructor);
+
+
+//const navbar = ({company}) => {}
+// this called de structure
+
+// {
+
+//"name": "hitesh",
+//"coursename": "js in hindi",
+//"price": "free",
+
+//}
+
+[
+    {},
+    {},
+    {}
+]
+
